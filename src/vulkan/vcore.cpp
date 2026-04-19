b609fd652bc37e9830fba20bcab3d8fed2367337
mwinter02 <90807019+mwinter02@users.noreply.github.com>
2026-04-18 22:33:54 -0400

    RecreateBridge method for creating new buffer when resizing

--- src/vulkan/vcore.cpp
+++ src/vulkan/vcore.cpp
@@ -8,10 +8,17 @@ namespace VSTIR {
 
     void VCore::InitializeBridge() {
         VUTILS::CreateBuffer(
-            _width * _height * 4, VK_BUFFER_USAGE_TRANSFER_DST_BIT,
+            _render_width * _render_height * 4, VK_BUFFER_USAGE_TRANSFER_DST_BIT,
             VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_CACHED_BIT,
             &m_Bridge);
         vkMapMemory(_interface, m_Bridge.memory, 0, VK_WHOLE_SIZE, 0, &(_swapchain.reference));
+    }
+
+    void VCore::RecreateBridge() {
+        vkUnmapMemory(_interface, m_Bridge.memory);
+        VUTILS::DestroyBuffer(m_Bridge);
+        m_Bridge = {};
+        InitializeBridge();
     }
 
     void VCore::Reconstruct() {

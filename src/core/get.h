a508f57ecfa6dde0a8738e56a4dbfa2fc8461add
mwinter02 <90807019+mwinter02@users.noreply.github.com>
2026-04-18 22:31:19 -0400

    viewport, render and camera getters

--- src/core/get.h
+++ src/core/get.h
@@ -9,9 +9,15 @@
 #define _gpu (Editor::Get()->GetRenderer().GetBackend().Core().General().GPU())
 #define _width (Editor::Get()->Width())
 #define _height (Editor::Get()->Height())
+#define _viewport_width_ratio (Editor::Get()->ViewportWidthRatio())
+#define _viewport_height_ratio (Editor::Get()->ViewportHeightRatio())
+#define _viewport_width (Editor::Get()->ViewportWidth())
+#define _viewport_height (Editor::Get()->ViewportHeight())
 
-// #define _render_width
-// TODO Viewport dimensions, wire in
+#define _camera (Editor::Get()->GetRenderer().GetCamera())
+
+#define _render_width  ((uint32_t)std::max<size_t>(1, (size_t)(_viewport_width  * _render_settings.resolution_scale)))
+#define _render_height ((uint32_t)std::max<size_t>(1, (size_t)(_viewport_height * _render_settings.resolution_scale)))
 #define _swapchain (Editor::Get()->GetRenderer().Swapchain())
 #define _context (Editor::Get()->GetRenderer().GetBackend().Core().Context())
 #define _shaders (Editor::Get()->GetRenderer().GetBackend().Core().Shaders())

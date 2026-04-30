e320b44d8e11fd3481c86da5eb43073023f93ff4
mwinter02 <90807019+mwinter02@users.noreply.github.com>
2026-04-30 18:54:09 -0400

    Performance improvements for bvh

--- src/util/bvh.cpp
+++ src/util/bvh.cpp
@@ -1,164 +1,117 @@
 #include "bvh.h"
 #include "util/log.h"
 
+#include <algorithm>
+#include <limits>
+
 #define BVH_LIMIT 0.0001f
 
 namespace VSTIR {
 
-    void ResizeBVH(std::vector<NodeBVH>& bvh, size_t index) {
-        if (bvh[index].config == BVH_BOTH) {
-            ResizeBVH(bvh, bvh[index].left);
-            ResizeBVH(bvh, bvh[index].right);
-            bvh[index].min = glm::min(bvh[bvh[index].left].min, bvh[index].min);
-            bvh[index].max = glm::max(bvh[bvh[index].left].max, bvh[index].max);
-            bvh[index].min = glm::min(bvh[bvh[index].right].min, bvh[index].min);
-            bvh[index].max = glm::max(bvh[bvh[index].right].max, bvh[index].max);
-        } else if (bvh[index].config == BVH_LEFT_ONLY) {
-            ResizeBVH(bvh, bvh[index].left);
-            bvh[index].min = glm::min(bvh[bvh[index].left].min, bvh[index].min);
-            bvh[index].max = glm::max(bvh[bvh[index].left].max, bvh[index].max);
-        } else if (bvh[index].config == BVH_RIGHT_ONLY) {
-            ResizeBVH(bvh, bvh[index].right);
-            bvh[index].min = glm::min(bvh[bvh[index].right].min, bvh[index].min);
-            bvh[index].max = glm::max(bvh[bvh[index].right].max, bvh[index].max);
-        } else {
-            return;
-        }
-    }
-
-    void SplitBVH(std::vector<NodeBVH>& bvh, size_t index, const std::vector<Triangle>& triangles, std::vector<size_t>& children, const std::vector<AABB> aabbs) {
-        #define CBVH bvh[index]
-        #define BVHMIN bvh[index].min
-        #define BVHMAX bvh[index].max
-
-        // split boxes
-        float xyz_dist[3] = {
-            BVHMAX.x - BVHMIN.x,
-            BVHMAX.y - BVHMIN.y,
-            BVHMAX.z - BVHMIN.z
-        };
-        int dist_ind = xyz_dist[0] > xyz_dist[1] ?
-            (xyz_dist[0] > xyz_dist[2] ? 0 : 2) :
-            (xyz_dist[1] > xyz_dist[2] ? 1 : 2);
-        if (xyz_dist[dist_ind] < BVH_LIMIT) {
-            size_t stream_index = index;
-            for (size_t i = 0; i < children.size(); i++) {
-                NodeBVH child = {
-                    aabbs[children[i]].min,
-                    aabbs[children[i]].max,
-                    BVH_LEAF, (uint32_t)children[i], 0
-                };
-                bvh.push_back(child);
-                bvh[stream_index].left = bvh.size() - 1;
-
-                if (i + 1 >= children.size()) {
-                    bvh[stream_index].config = BVH_LEFT_ONLY;
-                } else {
-                    bvh[stream_index].config = BVH_BOTH;
-                    NodeBVH next = { BVHMIN, BVHMAX, BVH_LEAF, 0, 0 };
-                    bvh.push_back(next);
-                    bvh[stream_index].right = bvh.size() - 1;
-                    stream_index = bvh.size() - 1;
-                }
-            }
-            ResizeBVH(bvh, index);
-            return;
-        }
-        float mid_dist = xyz_dist[dist_ind] / 2.0f;
-        NodeBVH left = { BVHMIN, BVHMAX, BVH_LEAF, 0, 0 };
-        NodeBVH right = { BVHMIN, BVHMAX, BVH_LEAF, 0, 0 };
-        left.max[dist_ind] -= mid_dist;
-        right.min[dist_ind] += mid_dist;
-
-        // subchildren
-        std::vector<size_t> left_children;
-        std::vector<size_t> right_children;
-        for (size_t i = 0; i < children.size(); i++) {
-            if (aabbs[children[i]].centroid[dist_ind] < left.max[dist_ind])
-                left_children.push_back(children[i]);
-            else
-                right_children.push_back(children[i]);
-        }
-        if (left_children.size() > 0 && right_children.size() > 0) {
-            CBVH.config = BVH_BOTH;
-        } else if (left_children.size() > 0) {
-            CBVH.config = BVH_LEFT_ONLY;
-        } else if (right_children.size() > 0) {
-            CBVH.config = BVH_RIGHT_ONLY;
-        } else {
-            FATAL("This bvh logic should never happen");
-            CBVH.config = BVH_LEAF;
-        }
-        if (left_children.size() > 1) {
-            bvh.push_back(left);
-            CBVH.left = bvh.size() - 1;
-            SplitBVH(bvh, CBVH.left, triangles, left_children, aabbs);
-        } else if (left_children.size() == 1) {
-            left.config = BVH_LEAF;
-            left.left = left_children[0];
-            left.min = aabbs[left.left].min;
-            left.max = aabbs[left.left].max;
-            bvh.push_back(left);
-            CBVH.left = bvh.size() - 1;
-        }
-        if (right_children.size() > 1) {
-            bvh.push_back(right);
-            CBVH.right = bvh.size() - 1;
-            SplitBVH(bvh, CBVH.right, triangles, right_children, aabbs);
-        } else if (right_children.size() == 1) {
-            right.config = BVH_LEAF;
-            right.left = right_children[0];
-            right.min = aabbs[right.left].min;
-            right.max = aabbs[right.left].max;
-            bvh.push_back(right);
-            CBVH.right = bvh.size() - 1;
-        }
-
-        // resize
-        if (bvh[index].config == BVH_BOTH || bvh[index].config == BVH_LEFT_ONLY) {
-            bvh[index].min = glm::min(bvh[bvh[index].left].min, bvh[index].min);
-            bvh[index].max = glm::max(bvh[bvh[index].left].max, bvh[index].max);
-        }
-        if (bvh[index].config == BVH_BOTH || bvh[index].config == BVH_RIGHT_ONLY) {
-            bvh[index].min = glm::min(bvh[bvh[index].right].min, bvh[index].min);
-            bvh[index].max = glm::max(bvh[bvh[index].right].max, bvh[index].max);
-        }
-
-        #undef CBVH
-        #undef BVHMIN
-        #undef BVHMAX
-    }
-
-    std::vector<AABB> generateAABBs(const std::vector<Triangle>& triangles, const std::vector<glm::vec4>& vertices) {
+    static std::vector<AABB> generateAABBs(const std::vector<Triangle>& triangles, const std::vector<glm::vec4>& vertices) {
         std::vector<AABB> aabbs;
-        for (int i = 0; i < triangles.size(); i++) {
-            glm::vec3 a = glm::vec3(vertices[triangles[i].a]);
-            glm::vec3 b = glm::vec3(vertices[triangles[i].b]);
-            glm::vec3 c = glm::vec3(vertices[triangles[i].c]);
-            glm::vec3 min = glm::min(a, glm::min(b, c));
-            glm::vec3 max = glm::max(a, glm::max(b, c));
-            glm::vec3 centroid = (min + max) / 2.0f;
+        aabbs.reserve(triangles.size());
+        for (size_t i = 0; i < triangles.size(); i++) {
+            const glm::vec3 a = glm::vec3(vertices[triangles[i].a]);
+            const glm::vec3 b = glm::vec3(vertices[triangles[i].b]);
+            const glm::vec3 c = glm::vec3(vertices[triangles[i].c]);
+            const glm::vec3 min = glm::min(a, glm::min(b, c));
+            const glm::vec3 max = glm::max(a, glm::max(b, c));
+            const glm::vec3 centroid = (min + max) * 0.5f;
             aabbs.push_back((AABB){ min, max, centroid });
         }
         return aabbs;
     }
 
+    static int LongestAxis(const glm::vec3& extents) {
+        return extents.x > extents.y ? (extents.x > extents.z ? 0 : 2) : (extents.y > extents.z ? 1 : 2);
+    }
+
+    static void ComputeRangeBounds(
+        const std::vector<size_t>& indices,
+        const std::vector<AABB>& aabbs,
+        size_t begin,
+        size_t end,
+        glm::vec3& outMin,
+        glm::vec3& outMax,
+        glm::vec3& centroidMin,
+        glm::vec3& centroidMax) {
+        outMin = glm::vec3(std::numeric_limits<float>::max());
+        outMax = glm::vec3(-std::numeric_limits<float>::max());
+        centroidMin = glm::vec3(std::numeric_limits<float>::max());
+        centroidMax = glm::vec3(-std::numeric_limits<float>::max());
+        for (size_t i = begin; i < end; i++) {
+            const AABB& box = aabbs[indices[i]];
+            outMin = glm::min(outMin, box.min);
+            outMax = glm::max(outMax, box.max);
+            centroidMin = glm::min(centroidMin, box.centroid);
+            centroidMax = glm::max(centroidMax, box.centroid);
+        }
+    }
+
     std::vector<NodeBVH> BVH::Create(const std::vector<Triangle>& triangles, const std::vector<glm::vec4>& vertices) {
+        if (triangles.empty()) {
+            return {};
+        }
+
         std::vector<NodeBVH> bvh;
-        std::vector<AABB> aabbs = generateAABBs(triangles, vertices);
-        NodeBVH root = (NodeBVH){
-            glm::vec3(std::numeric_limits<float>::max()),
-            glm::vec3(-std::numeric_limits<float>::max()),
-            BVH_LEAF, 0, 0
-        };
+        bvh.reserve(triangles.size() * 2);
+
+        const std::vector<AABB> aabbs = generateAABBs(triangles, vertices);
         std::vector<size_t> indices;
+        indices.reserve(triangles.size());
         for (size_t i = 0; i < triangles.size(); i++) {
-            root.min = glm::min(aabbs[i].min, root.min);
-            root.max = glm::max(aabbs[i].max, root.max);
             indices.push_back(i);
         }
-        bvh.push_back(root);
-        SplitBVH(bvh, 0, triangles, indices, aabbs);
+
+        auto build = [&](auto&& self, size_t begin, size_t end) -> uint32_t {
+            glm::vec3 nodeMin;
+            glm::vec3 nodeMax;
+            glm::vec3 centroidMin;
+            glm::vec3 centroidMax;
+            ComputeRangeBounds(indices, aabbs, begin, end, nodeMin, nodeMax, centroidMin, centroidMax);
+
+            const uint32_t nodeIndex = (uint32_t)bvh.size();
+            bvh.push_back((NodeBVH){ nodeMin, nodeMax, BVH_LEAF, 0, 0 });
+
+            const size_t count = end - begin;
+            if (count == 1) {
+                bvh[nodeIndex].config = BVH_LEAF;
+                bvh[nodeIndex].left = (uint32_t)indices[begin];
+                bvh[nodeIndex].right = 0;
+                return nodeIndex;
+            }
+
+            const glm::vec3 centroidExtents = centroidMax - centroidMin;
+            const int axis = LongestAxis(centroidExtents);
+            const size_t mid = begin + count / 2;
+
+            if (centroidExtents[axis] > BVH_LIMIT) {
+                std::nth_element(
+                    indices.begin() + begin,
+                    indices.begin() + mid,
+                    indices.begin() + end,
+                    [&](size_t a, size_t b) {
+                        return aabbs[a].centroid[axis] < aabbs[b].centroid[axis];
+                    });
+            }
+
+            // Force progress even in degenerate centroid distributions.
+            if (mid == begin || mid == end) {
+                const size_t fallbackMid = begin + count / 2;
+                bvh[nodeIndex].config = BVH_BOTH;
+                bvh[nodeIndex].left = self(self, begin, fallbackMid);
+                bvh[nodeIndex].right = self(self, fallbackMid, end);
+                return nodeIndex;
+            }
+
+            bvh[nodeIndex].config = BVH_BOTH;
+            bvh[nodeIndex].left = self(self, begin, mid);
+            bvh[nodeIndex].right = self(self, mid, end);
+            return nodeIndex;
+        };
+
+        build(build, 0, indices.size());
         return bvh;
     }
 
